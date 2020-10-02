be1ad01ca1ce906d76526ff2c6173d68ff8da00f
Angga Ramadhan <surel.angga@gmail.com>
2020-10-02 20:26:47 +0700

    Initial Commit

---
+++ DotaHeroes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DotaHeroes
+//
+//  Created by BRI on 02/10/20.
+//  Copyright © 2020 Angga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
